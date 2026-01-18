049ef621d5078d8a8f89110d5e57d60b2d76b0c8
romangorbovets <romangorbovets@icloud.com>
2026-01-18 15:09:34 +0300

    Update DependencyInjection.cs

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -2,18 +2,13 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ReservationService.Application;
 
-/// <summary>
-/// Методы расширения для регистрации сервисов Application слоя
-/// </summary>
+
 public static class DependencyInjection
 {
-    /// <summary>
-    /// Регистрирует сервисы Application слоя
-    /// </summary>
+    
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        // Здесь будут регистрироваться сервисы Application слоя
-        // Например: репозитории, сервисы приложения, валидаторы и т.д.
+       
 
         return services;
     }
